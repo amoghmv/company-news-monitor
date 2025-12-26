2752a5e877a3801ec24b04d0ea5507924c44a08d
amoghmv <amoghmv888@gmail.com>
2025-12-27 03:05:29 +0530

    Update news_monitor.py

--- news_monitor.py
+++ news_monitor.py
@@ -84,7 +84,7 @@ def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     payload = {
         "chat_id": chat_id,
-        "text": message.
+        "text": messagem,
         "parse_mode": "HTML"
     }
 
